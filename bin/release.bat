e458e14d70b8a45c2f348fe0b23d678a2d701ad0
maurice <es.chenxiaobo@gmail.com>
2014-02-10 16:20:22 +0800

    修改release.bat文件.

--- bin/release.bat
+++ bin/release.bat
@@ -12,19 +12,11 @@ call mvn release:clean
 call mvn release:prepare
 call mvn release:perform
 
-echo [INFO] create base curd project archetype
-cd %base_path%\showcase\base-curd
-call mvn archetype:create-from-project
+cd %base_path%
+echo [INFO] push to github
+call git push origin master
 
-echo [INFO] release base curd project archetype
-cd %base_path%\showcase\base-curd\target\generated-sources\archetype
-call mvn clean install -Dmaven.test.skip=true
-call mvn release:clean
-call mvn release:prepare
-call mvn release:perform
-
-echo [INFO] delete base curd project generated-sources target
-cd %base_path%\showcase\base-curd
-rd /S /Q target
+echo [INFO] push tag to github
+call git push --tags
 
 pause
