e2d8984a8587625f28e6bd04e07af03f33e1d9e7
maurice <es.chenxiaobo@gmail.com>
2014-02-10 13:26:25 +0800

    修改release.bat文件

--- bin/release.bat
+++ bin/release.bat
@@ -9,25 +9,15 @@ set base_path=%cd%
 
 echo [INFO] release clean project
 call mvn release:clean
-
-echo [INFO] release prepare project
 call mvn release:prepare
-
-echo [INFO] release perform dactiv-parent project
-cd %base_path%\dactiv-parent
 call mvn release:perform
 
-echo [INFO] release perform dactiv-common project
-cd %base_path%\dactiv-common
-call mvn release:perform
+cd %base_path%
+echo [INFO] push to github
+call git push origin master
 
-echo [INFO] release perform dactiv-orm project
-cd %base_path%\dactiv-orm
-call mvn release:perform
-
-echo [INFO] create base curd project archetype
-cd %base_path%\showcase\base-curd
-call mvn archetype:create-from-project
+echo [INFO] push tag to github
+call git push --tags
 
 echo [INFO] release base curd project archetype
 cd %base_path%\showcase\base-curd\target\generated-sources\archetype
@@ -39,12 +29,5 @@ call mvn release:perform
 echo [INFO] delete base curd project generated-sources target
 cd %base_path%\showcase\base-curd
 rd /S /Q target
-
-cd %base_path%
-echo [INFO] push to github
-call git push origin master
-
-echo [INFO] push tag to github
-call git push --tags
 
 pause
